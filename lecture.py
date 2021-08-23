dc2045af3ba499af24727c37b92145dc55119aa0
Farooq Butt <farooqbutt548@gmail.com>
2021-08-23 15:56:58 +0500

    Loops

--- lecture.py
+++ lecture.py
@@ -35,10 +35,10 @@ if 7 not in name:
     print("it's true statement") '''
 
 
-# ---Loops
+                # ---Loops
 #  we can get output of while_loop with for_loop and vise versa
 
-# --simple while_loops
+                        # -- while_loops
 '''num = int(input('enter a n_num to their sum : '))
 total = 0
 i = 0
@@ -64,3 +64,25 @@ while i < len(name):
         temp_cell += name[i]
         print(f'the char {name[i]} is {name.count(name[i])} times in {name}')
     i +=1'''
+                # ---for_loop
+
+# sum via slicing
+'''total = 0
+number = input('plz enter a num to addition : ')
+for i in range(0, len(number)):
+    total += int(number[i])
+print(total)'''
+# sum of n_num with For_loop
+'''num = int(input('plz enter a n_num to addition : '))
+total = 0
+for i in range(0,num+1):
+    total+= i
+print(total)'''
+# letter_count with for loop
+'''name = input('enter name to letter count : ')
+cell = ''
+for i in range(len(name)):
+    if name[i] not in cell:
+        cell += name[i]
+        print(f'the "{name[i]}" in "{name} : {name.count(name[i])}" timkes.')'''
+        
