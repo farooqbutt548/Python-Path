4b14b4dc7b80d6ef704c2ec8f29d4734b463d086
Farooq Butt <farooqbutt548@gmail.com>
2021-08-25 17:09:02 +0500

    Numbers

--- lecture.py
+++ lecture.py
@@ -85,4 +85,19 @@ for i in range(len(name)):
     if name[i] not in cell:
         cell += name[i]
         print(f'the "{name[i]}" in "{name} : {name.count(name[i])}" timkes.')'''
-        
+
+                                #Numbers 
+#integer Numbers
+int_nums =50
+float_nums = 3.1428
+complex_nums = 2+3j
+print(type(int_nums)) # interger
+print(type(float_nums)) # float 
+print(type(complex_nums)) # complex
+print(2+2)  #integer
+print(type(2+2.33))  #flout
+print(f"we can type int, float here {2+2}, .... ")
+new_float = int_nums+float_nums
+print(new_float)
+print(type(new_float))  #  float
+
